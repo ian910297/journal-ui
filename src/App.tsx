f00762aa43598e017f0d8ed7e7e4e04842242175
chung-yi.chi <ian910297@gmail.com>
2025-10-08 07:08:05 +0800

    delete create post
    Signed-off-by: chung-yi.chi <ian910297@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import CreatePost from './pages/CreatePost';
 import Whoami from './pages/Whoami';
 import BlogPostList from './pages/BlogPostList';
 import BlogPost from './pages/BlogPost';
@@ -13,7 +12,6 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/create" element={<CreatePost />} />
           <Route path="/whoami" element={<Whoami />} />
           <Route path="/posts" element={<BlogPostList />} />
           <Route path="/posts/:id" element={<BlogPost />} />
